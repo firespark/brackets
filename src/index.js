5e303c835275a2075d587e68f7787d7f63dc2c78
fireleit <fireleit@gmail.com>
2024-07-29 19:57:55 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let itemsArr = [];
+  let correctN = 0;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    itemsArr[i] = bracketsConfig[i].join('');
+  }
+
+  let ctr = 0;
+  let j = 0;
+
+  while(ctr < itemsArr.length) {
+      while (j < str.length) {
+        correctN = str.indexOf(itemsArr[ctr]);
+          if (correctN !== -1){
+              str = str.substring(0, correctN) + str.substring(correctN + itemsArr[ctr].length, str.length);
+              ctr = 0;
+          } else {
+              j = str.length;
+          }
+      }
+      ctr++;
+      j = 0;
+  }
+  return (!str);
+};
